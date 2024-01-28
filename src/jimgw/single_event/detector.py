fa12701802786af1ce4ffd21016fe36bc567f98b
Tsun Ho Pang <tsun-ho.pang@ligo.org>
2024-01-28 08:31:11 -0800

    Update the url for ET-D psd

--- src/jimgw/single_event/detector.py
+++ src/jimgw/single_event/detector.py
@@ -19,7 +19,7 @@ psd_file_dict = {
     "H1": "https://dcc.ligo.org/public/0169/P2000251/001/O3-H1-C01_CLEAN_SUB60HZ-1251752040.0_sensitivity_strain_asd.txt",
     "L1": "https://dcc.ligo.org/public/0169/P2000251/001/O3-L1-C01_CLEAN_SUB60HZ-1240573680.0_sensitivity_strain_asd.txt",
     "V1": "https://dcc.ligo.org/public/0169/P2000251/001/O3-V1_sensitivity_strain_asd.txt",
-    "ET-D": "https://git.ligo.org/lscsoft/bilby/-/blob/master/bilby/gw/detector/noise_curves/ET_D_psd.txt"
+    "ET-D": "https://git.ligo.org/lscsoft/bilby/-/raw/master/bilby/gw/detector/noise_curves/ET_D_psd.txt"
 }
 
 
